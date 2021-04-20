2e53638748edd5794627867119ddfd8ab911f3c9
Elon Salfati <elon@salfati.io>
2021-04-20 14:04:44 +0300

    fixed typo in cleanup script

--- template/scripts/cleanup.sh
+++ template/scripts/cleanup.sh
@@ -4,8 +4,8 @@
 WORKING_DIR=$(pwd)
 
 # deactivate workflows
-rb workflow:deactivate search_issue -d user
-rb workflow:deactivate create_issue -d user
+rb workflow:deactivate jira_search_issue -d user
+rb workflow:deactivate jira_create_issue -d user
 
 # remove configurations
 rb delete -f "$WORKING_DIR/configs"
