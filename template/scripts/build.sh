b4e05467fce63e056326ba5f95f9d1ae3411461d
Saar Wintrov <saarwintrov@Saars-MacBook-Pro.local>
2021-05-23 16:32:21 +0300

    updated dockerfile k8s file

--- template/scripts/build.sh
+++ template/scripts/build.sh
@@ -12,21 +12,20 @@ WORKING_DIR=$(pwd)
 
 if [[ "$project" && $region && $cluster_name ]]; then
     # build image
-    # docker build . -t jira -f "$WORKING_DIR/Dockerfile"
+    docker build . -t jira -f "$WORKING_DIR/Dockerfile"
     # change image name
-    # docker tag jira:latest eu.gcr.io/$project/jira
+    docker tag jira:latest eu.gcr.io/$project/jira
     # docker push
-    image_name=eu.gcr.io/$project/jira
-    echo $image_name
-    # docker push $image_name
+    docker push "eu.gcr.io/$project/jira"
     # rb tool
-    # rb auth:login
+    rb auth:login
     # gcloud cluster login
-    # gcloud container clusters get-credentials $cluster_name --region $region --project $region
+    gcloud container clusters get-credentials $cluster_name --region $region --project $project
     # rb tool apply
-    # rb apply -f configs
+    rb apply -f configs
     # placeholder replace
-    sed -i'.bkp' "s/IMAGE_NAME_PLACEHOLDER/$image_name/g" "$WORKING_DIR/k8s/jira.yaml"
-    # k8s deployment / vs / service / gw
+    IMAGE_NAME="eu.gcr.io\/$project\/jira"
+    sed -i'.bkp' -e "s/PLACEHOLDER_IMAGE_NAME/$IMAGE_NAME/g" "$WORKING_DIR/k8s/jira.yaml"
+    #k8s deployment / vs / service / gw
     kubectl apply -f "$WORKING_DIR/k8s/jira.yaml"
 fi
